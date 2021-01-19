a3597e9e89f67ec29daf27e9b67fa8420cf87f93
Bharat Maheshwari <59102335+BharatMaheshwari96@users.noreply.github.com>
2021-01-19 22:35:22 +0000

    Add files via upload

---
+++ test.py
@@ -0,0 +1,6 @@
+from functools import reduce
+number = int(input("Enter the number of input:"))
+for i in range(number):
+    s= input()
+    Sum = int(str(reduce(lambda x,y: x+y, map(int, [e[::-1] for e in s.split()])))[::-1])
+    print(Sum)
