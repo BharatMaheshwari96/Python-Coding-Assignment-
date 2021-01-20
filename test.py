d72011d54844ca10af67feb254255b9ac088203a
Bharat Maheshwari <59102335+BharatMaheshwari96@users.noreply.github.com>
2021-01-20 12:01:24 +0000

    Update test.py

--- test.py
+++ test.py
@@ -2,5 +2,9 @@ from functools import reduce
 number = int(input("Enter the number of input:")) 
 for i in range(number):
     s= input() # taking input values in string format
-    Sum = int(str(reduce(lambda x,y: x+y, map(int, [e[::-1] for e in s.split()])))[::-1])
-    print(Sum)
+    # If condition to check wheter the given input is size of 2 or not and print 0 if no input is given
+    if len(s.split()) == 2:
+        Sum = int(str(reduce(lambda x,y: x+y, map(int, [e[::-1] for e in s.split()])))[::-1])
+        print(Sum)
+    else:
+        print("0")
